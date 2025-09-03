ca8ab64ade18c432dc1b9e21ec1d6e5c7df64531
Br <131480135+brzzera@users.noreply.github.com>
2025-09-03 16:54:11 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,36 +1,34 @@
 const steamUser = require('steam-user');
-const steamTotp = require('steam-totp');
-const keep_alive = require('./keep_alive.js')
+const keep_alive = require('./keep_alive.js');
 
-var username = process.env.username;
-var password = process.env.password;
-var shared_secret = process.env.shared;
+// Coloque seus cookies de sessão aqui ou use variáveis de ambiente
+const sessionid = process.env.SESSIONID || 'SEU_SESSIONID';
+const steamLoginSecure = process.env.STEAMLOGINSECURE || 'SEU_STEAMLOGINSECURE';
 
-var games = [730, 440, 570];  // Enter here AppIDs of the needed games
-var status = 1;  // 1 - online, 7 - invisible
+// Jogos que você quer idlar
+const games = [730, 440, 570];  // AppIDs
+const status = 1;  // 1 = online, 7 = invisible
 
+const user = new steamUser();
 
-user = new steamUser();
-user.logOn({"accountName": username, "password": password, "twoFactorCode": steamTotp.generateAuthCode(shared_secret)});
-user.on('loggedOn', () => {
-	if (user.steamID != null) console.log(user.steamID + ' - Successfully logged on');
-	user.setPersona(status);               
-	user.gamesPlayed(games);
+// Logon usando sessão web
+user.logOn({
+    accountName: null,   // não precisa de username
+    webLogOn: true,
+    machineName: 'SimpleIdler',
+    webSession: {
+        sessionid: sessionid,
+        steamLoginSecure: steamLoginSecure
+    }
 });
 
+user.on('loggedOn', () => {
+    if (user.steamID != null) console.log(user.steamID + ' - Successfully logged on via session cookies');
+    user.setPersona(status);
+    user.gamesPlayed(games);
+});
 
-// var username2 = process.env.username2;
-// var password2 = process.env.password2;
-// var shared_secret2 = process.env.shared2;
-
-// var games2 = [730, 440, 570, 304930];  // Enter here AppIDs of the needed games
-// var status2 = 1;  // 1 - online, 7 - invisible
-
-
-// user2 = new steamUser();
-// user2.logOn({"accountName": username2, "password": password2, "twoFactorCode": steamTotp.generateAuthCode(shared_secret2)});
-// user2.on('loggedOn', () => {
-// 	if (user2.steamID != null) console.log(user2.steamID + ' - Successfully logged on');
-// 	user2.setPersona(status2);               
-// 	user2.gamesPlayed(games2);
-// });
+// Eventos de erro para debug
+user.on('error', (err) => {
+    console.error('Erro ao logar:', err);
+});
